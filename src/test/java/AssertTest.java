ffb37695694799947bb71e1781e29959d820f0d3
Shouzeb <shouzebhasan99@gmail.com>
2020-08-05 00:57:46 +0500

    adding tests

---
+++ src/test/java/AssertTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(org.junit.runners.Suite.class)
+
+
+@org.junit.runners.Suite.SuiteClasses({
+        IEEETestAutomationLogin.class,
+        IEEETestAutomationSignUp.class
+})
+
+public class AssertTest {
+}
